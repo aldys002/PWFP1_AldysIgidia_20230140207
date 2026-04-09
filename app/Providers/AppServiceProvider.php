7c393448d3ddff9dc9d5a6edf022906c8e39020f
aldys002 <igidiaaldys@gmail.com>
2026-04-09 22:33:08 +0700

    membuat export hanya untuk admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider; // Tambahkan ini
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Gate untuk tombol Export (Hanya Admin)
+        // Sesuai tugas Kelas B
+        Gate::define('export-product', function (User $user) {
+            return $user->role === 'admin';
+        });
     }
 }
