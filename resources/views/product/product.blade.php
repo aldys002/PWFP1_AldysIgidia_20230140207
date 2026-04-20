c8848edd699662995c65503f3f3de7f0a7f87f87
aldys002 <igidiaaldys@gmail.com>
2026-04-20 22:46:26 +0700

    {{ $url }} → link dinamis {{ $name }} → teks dinamis Bisa digunakan ulang untuk berbagai data

---
+++ resources/views/product/product.blade.php
@@ -0,0 +1,8 @@
+
+    <a href="{{ $url }}" 
+       class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 transition ease-in-out duration-150">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+        </svg>
+        Add {{ $name }}
+    </a>
